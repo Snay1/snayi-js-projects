71640f4281de1673d99cce92e68940a12688b05b
Snay1 <stpshk767@gmail.com>
2022-06-07 18:18:35 +0300

    snayi-js-projects

---
+++ AudioVisualiser/script.js
@@ -0,0 +1,135 @@
+﻿let analyser, context, arrayTiming;
+let switchDegrees = true;
+const fullLogo = document.querySelector('.logo')
+const logo = document.querySelector('#logo');
+const audio = document.querySelector('#audio');
+const logoItem = document.querySelectorAll('.logo__item');
+const bodyBorder = document.querySelectorAll('.body__border');
+
+const volumeControl = document.querySelector('.volumeControl');
+
+const playButton = document.querySelector('.play');
+
+const controls = document.querySelector('.controls');
+const showControls = document.querySelector('.show__controls');
+const volumeLine = document.querySelector('.volume__line')
+
+const audioLine = document.querySelector('.audio__line');
+const durationLine = document.querySelector('.duration__line');
+const audioLineControl = document.querySelector('.audioLineControl');
+
+const currentTimeStat = document.querySelector('.time__current');
+const durationTimeStat = document.querySelector('.time__duration');
+
+
+window.onload = function() {
+    const audioDuration = Number(audio.duration);
+    let audioDurationSec = undefined;
+    const currentTime = Number(audio.currentTime);
+    currentTimeStat.innerText = audio.currentTime;
+    Math.trunc(audioDuration%60)>10 ? audioDurationSec = Math.trunc(audioDuration%60) : audioDurationSec = '0' + Math.trunc(audioDuration%60)
+    durationTimeStat.innerText = `${Math.floor(audioDuration/60)}:${audioDurationSec}`;
+    setInterval(function() {
+        switchDegrees = !switchDegrees;
+    }, arrayTiming)
+} 
+
+showControls.onclick = function() {
+    controls.classList.toggle('hide');
+    showControls.classList.toggle('rotate');
+    audioLine.classList.toggle('hide');
+}
+
+playButton.onclick = function() {
+    if(!context) {
+        preparation();
+    }
+    if(audio.paused) {
+        audio.play();
+        loop()
+        this.classList.add('playing');
+    }
+    else {
+        audio.pause();
+        this.classList.remove('playing');
+    }
+
+}
+
+function preparation() {
+    context = new AudioContext();
+    analyser = context.createAnalyser();
+    const src = context.createMediaElementSource(audio);
+    src.connect(analyser);
+    analyser.connect(context.destination);
+    loop();
+}
+function loop() {
+    if(!audio.paused) {
+        window.requestAnimationFrame(loop);
+    }
+    const array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(array);
+    arrayTiming = array[120]
+    for(logoEl of logoItem) {
+        logoEl.style.height = (array[60]/1.5)+ '%';
+        logoEl.style.width = (array[60]/8)+ '%';
+        logoEl.style.background  = `radial-gradient(circle, rgba(${array[55]-25},${array[30]-25},${array[60]-25},1) 0%, rgba(${array[60]},${array[15]},${array[40]},1) 54%)`;
+    }
+    logo.style.height = (array[60]/2)+ '%';
+    logo.style.width = (array[60]/2) + '%';
+    logo.style.background  = `radial-gradient(circle, rgba(${array[40]},${array[50]},${array[60]},1) ${array[70]/4}%, rgba(${array[40]},${array[60]},${array[50]},1) ${array[60]/2}%)`;
+    logo.style.boxShadow = `0 0 ${array[70]}px rgb(${array[40]},${array[50]},${array[60]})`;
+    for(bodyBorderItem of bodyBorder) {
+        bodyBorderItem.style.boxShadow = `0 0 ${array[70]/2}px rgb(${array[40]},${array[50]},${array[60]})`;
+    }
+
+    if(switchDegrees) {
+        fullLogo.style.transform = `rotate(${Number(array[40])}deg)`;
+    }
+    else {
+        fullLogo.style.transform = `rotate(-${Number(array[40])}deg)`;
+    }
+
+    let currentTime = Number(audio.currentTime);
+    const audioDuration = Number(audio.duration);
+    let audioDurationSec = undefined;
+    let currentTimeSec = undefined;
+    Math.trunc(audioDuration%60)>10 ? audioDurationSec = Math.trunc(audioDuration%60) : audioDurationSec = '0' + Math.trunc(audioDuration%60);
+    currentTime>60 ? currentTimeSec = Math.trunc(currentTime%60) : currentTimeSec = '0' + Math.trunc(currentTime%60);
+
+    if(Math.trunc(currentTime%60) >= 10) {
+        currentTimeSec = Math.trunc(currentTime%60);
+    }
+    else {
+        currentTimeSec = '0' + Math.trunc(currentTime%60);
+    }
+    if(currentTime >= audioDuration) {
+        playButton.classList.remove('playing');
+    }
+    durationLine.style.width = `${(currentTime/audioDuration)*100}%`;
+    currentTimeStat.innerText = `${Math.floor(currentTime/60)}:${currentTimeSec}`;
+    durationTimeStat.innerText = `${Math.floor(audioDuration/60)}:${audioDurationSec}`;
+
+}
+volumeControl.addEventListener('input', function(e) {
+    e.preventDefault();
+    audio.volume = volumeControl.value;
+    volumeLine.style.width = `${Number(volumeControl.value)*100}%`
+})
+
+audioLineControl.addEventListener('click', function(e) {
+    e.preventDefault();
+    const audioDuration = Number(audio.duration);
+    const audioControl = Number(audioLineControl.value);
+    let currentTime = audioControl*audioDuration;
+    audio.currentTime = currentTime;
+    if(!context) {
+        preparation();
+    }
+    if(audio.paused) {
+        audio.play();
+        loop()
+        playButton.classList.add('playing');
+    }
+})
---
+++ ClockJavaScript/main.js
@@ -0,0 +1,67 @@
+﻿function SimpleClock() {
+    const hoursArrow = document.querySelector('.hours');
+    const minutesArrow = document.querySelector('.minutes');
+    const secondsArrow = document.querySelector('.seconds');
+
+    const bodyBackground = document.querySelector('body');
+    const htmlBackground = document.querySelector('html');
+
+    const clockTitle = document.querySelector('.clock__title');
+    const status = document.querySelector('.status__text');
+
+    const degrees = 6;
+
+    setInterval(() => {
+        const date = new Date();
+
+        const hours = date.getHours() * 30;
+        const minutes = date.getMinutes() * degrees;
+        const seconds = date.getSeconds() * degrees;
+
+        hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
+        minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
+        secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
+        if (date.getHours() >= 12 && date.getHours() < 17) {
+            bodyBackground.style.background = '#9fb3cc';
+            htmlBackground.style.background = '#9fb3cc';
+            clockTitle.style.color = '#000'
+            status.style.color = '#000';
+        }
+        if (date.getHours() >= 17 && date.getHours() < 19){
+            bodyBackground.style.background = '#915f57';
+            htmlBackground.style.background = '#915f57';
+            clockTitle.style.color = '#fff'
+            status.style.color = '#fff';
+        }
+        if (date.getHours() >= 19 && date.getHours() < 24){
+            bodyBackground.style.background = '#4a4666';
+            htmlBackground.style.background = '#4a4666';
+            clockTitle.style.color = '#fff'
+        }
+        if (date.getHours() >= 0 && date.getHours < 6) {
+            bodyBackground.style.background = '#2e2d36';
+            htmlBackground.style.background = '#2e2d36';
+            clockTitle.style.color = '#fff'
+        }
+        if (date.getHours() >= 6 && date.getHours < 12) {
+            bodyBackground.style.background = '#cfd19d';
+            htmlBackground.style.background = '#cfd19d';
+            clockTitle.style.color = '#000'
+        }
+
+
+
+        if(date.getHours() >= 0 && date.getHours() < 12) {
+            status.innerHTML = 'Morning';
+        }
+        
+        if(date.getHours() >= 12 && date.getHours() < 18) {
+            status.innerHTML = 'Afternoon';
+        }
+        if(date.getHours() >= 18 && date.getHours() < 24) {
+            status.innerHTML = 'Evening';
+        }
+
+    }, 100)
+}
+SimpleClock()
